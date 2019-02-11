93cff350ff30bd9f2dbe8b74f62c6b58444cfb71
Gabriel Rapaport <gabrielrapaport@gmail.com>
2019-02-11 15:04:48 -0500

    dear lando

---
+++ labs/Lab04/board.cpp
@@ -0,0 +1,30 @@
+#include "board.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Board::Board()
+{
+    position = 0;
+}
+
+void Board::Play()
+{
+    cout << "Roll the Dice" << endl;
+}
+
+void Board::Winner()
+{
+    cout << "Dancing Time!" << endl;
+}
+
+void Board::setPosition(int p)
+{
+    position = p;
+}
+
+int Board::getPosition()
+{
+    return position;
+}
