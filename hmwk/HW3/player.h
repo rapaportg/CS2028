d0e653b19b35bf742324583e52d6793f451560bd
lslack <46945815+lslack@users.noreply.github.com>
2019-03-05 15:11:51 -0500

    Update player.h

--- hmwk/HW3/player.h
+++ hmwk/HW3/player.h
@@ -15,12 +15,12 @@ class Player
         Deck *deck;
         Player();
         void    initDeck();        // initializes first deck
-        int     deckSum();         // TODO: sums the value of the cards in deck for winner
+        int     deckSum();         // Lando::TODO: sums the value of the cards in deck for winner
         int     draw();            // DONE: player draws from top of deck
         void    bury();            // DONE: buries card at bottom of deck (note: will always bury entire hand)
         void    peekHand();        // Lando::TODO: Prints current hand for player
         void    saveCard();        // DONE: Places card from hand on top of side pile (note: will only save the card at hand[1])
         void    getFromPile();     // TODO: Moves card from top of side pile back to hand
-        bool    hasCards();        // TOOD: Checks to see if the player has any cards left in their deck and or pile
+        bool    hasCards();        // Lando::TOOD: Checks to see if the player has any cards left in their deck and or pile
 };
 #endif
