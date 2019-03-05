208788a8fdffb443453fa71fe0a19445b6ca1bb3
Gabe Rapaport <gabrielrapaport@gmail.com>
2019-03-05 11:36:20 -0500

    Update player.h

--- hmwk/HW3/player.h
+++ hmwk/HW3/player.h
@@ -15,12 +15,12 @@ class Player
 
     public:
         Player(std::string);
-        void initDeck();        // initializes first deck
-        int deckSum();          // TODO
-        int draw();             // TODO: player draws from top of deck
-        void bury();            // TODO: buries card at bottom of deck
-        void peekHand();        // TODO: Prints current hand for playervoid Deck::enqueue(int card) // insert front of link list
-        void saveCard();        // TODO: Places card from hand on top of side pile
-        void getFromPile();     // TODO: Moves card from top of side pile back to hand
+        void    initDeck();        // initializes first deck
+        int     deckSum();         // TODO
+        int     draw();            // TODO: player draws from top of deck
+        void    bury();            // TODO: buries card at bottom of deck
+        void    peekHand();        // Lando::TODO: Prints current hand for playervoid Deck::enqueue(int card) // insert front of link list
+        void    saveCard();        // TODO: Places card from hand on top of side pile
+        void    getFromPile();     // TODO: Moves card from top of side pile back to hand
 };
 #endif
