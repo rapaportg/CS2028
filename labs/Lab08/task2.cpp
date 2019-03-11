07e779381d8b6bc3c784854476438c898e9e4d76
lslack <46945815+lslack@users.noreply.github.com>
2019-03-11 19:49:49 -0400

    tests all functions added

--- labs/Lab08/task2.cpp
+++ labs/Lab08/task2.cpp
@@ -15,19 +15,23 @@ int main()
 {
 	int temp;
 	int *ptr;
+	int *ptr2;
 	Node<int> *myObject;
+	Node<int> *myObject2;
 	LinkList<int> myList;
 
-	for (int i = 0; i < 15; i++) {
+	for (int i = 0; i < 15; i++)
+	{
 		cout << "set value:";
 		cin >> temp;
+
 		ptr = new int(temp);
 		myObject = new Node<int>(ptr);
+
 		myList.addItem(myObject);
 		myList.printList();
 	}
 
-	
 	for (int j = 0; j < 15; j++)
 	{
 		cout << "what value do you want to remove? ";
@@ -36,9 +40,27 @@ int main()
 		ptr2 = new int(temp);
 		myObject2 = new Node<int>(ptr2);
 
-		cout << "Removing " << *(myList.getListItem(myObject2))->getItem() << " from the list " << endl;
-		myList.printList();
+		if (myList.isInList(myObject2))
+		{
+			cout << "Removing " << *(myList.getListItem(myObject2))->getItem() << " from the list " << endl;
+			myList.printList();
+		}
+		else
+		{
+			cout << "That item is not in the list! " << endl;
+		}
 	}
 
+	if (myList.isEmpty())
+	{
+		cout << "isEmpty() works! the list was empty " << endl;
+	}
+	else if (!myList.isEmpty())
+	{
+		cout << "isEmpty() works! the list wasn't empty " << endl;
+	}
+	cout << "The size of the list is: " << myList.Size() << endl;
+
+	return 0;
 }
 
