6994f4f85a5bd3819e76c5272555b33b66eaae2f
kurtdschmidt <47017068+kurtdschmidt@users.noreply.github.com>
2019-03-11 16:05:46 -0400

    Update task2.cpp

---
+++ labs/Lab08/task2.cpp
@@ -0,0 +1,28 @@
+// lab8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "linkList.h"
+#include "node.h"
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int temp;
+	int *ptr;
+	Node<int> *myObject;
+	LinkList<int> myList;
+	for (int i = 0; i < 6; i++) {
+		cout << "set value:";
+		cin >> temp;
+		ptr = new int(temp);
+		myObject = new Node<int>(ptr);
+		myList.addItem(myObject);
+		myList.printList();
+	}
+
+}
+
