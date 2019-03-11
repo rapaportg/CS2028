d403599de5751f664745206fa0046768036136c4
lslack <46945815+lslack@users.noreply.github.com>
2019-03-11 19:45:12 -0400

    added getListItem, isInList, isEmpty, and size functions

--- labs/Lab08/linkList.h
+++ labs/Lab08/linkList.h
@@ -10,17 +10,17 @@ template<class T>
 class LinkList {
 private:
 	Node<T> *head = nullptr;
-	int length=0;
+	int length = 0;
 	Node<T> *findParent(Node<T> *itemToAdd);
 public:
 	class NoListException {};
 	void addItem(Node<T> *itemToAdd);
+	Node<T>* getListItem(Node<T> *itemToGet);
+	bool isInList(Node<T> *itemToCheck);
+	bool isEmpty();
+	int Size();
 	void printList();
-
-
-
 };
-
 
 template<class T>
 void LinkList<T>::addItem(Node<T> *itemToAdd) {
@@ -64,6 +64,79 @@ Node<T> *LinkList<T>::findParent(Node<T> *itemToAdd) {
 	}
 	//cout << "Returning the ptr item val "<< *(ptr->getItem()) <<" with next adress "<< ptr->getNext() <<"\n";
 	return ptr;
+}
+
+template<class T>
+Node<T> *LinkList<T>::getListItem(Node<T> *itemToGet)
+{
+	Node<T> *tmp1 = head;
+	Node<T> *tmp2 = head->getNext();
+	if (*(head->getItem()) == *(itemToGet->getItem()))
+	{
+		head = head->getNext();		// if item is in the first spot in the list, head->next becomes new head, return old head
+		length--;
+		return tmp1;
+	}
+	else
+	{
+		do
+		{
+			if (*(tmp2->getItem()) == *(itemToGet->getItem()))	// if item is in the current spot being checked, prev spot's next becomes current spot's next
+			{							// return what's in the current spot
+				tmp1->setNext(tmp2->getNext());
+				length--;
+				return tmp2;
+			}
+			else
+			{
+				if (tmp2->getNext() == nullptr)	// if current spot's next is null, it's the end of the list; return null
+				{
+					return nullptr;
+				}
+				tmp2 = tmp2->getNext();		// advances our current and prev spots
+				tmp1 = tmp1->getNext();
+			}
+		} while (*(tmp1->getItem()) != *(itemToGet->getItem()));
+		return nullptr;
+	}
+}
+
+template<class T>
+bool LinkList<T>::isInList(Node<T> *itemToCheck)	// Checks for item in current spot, then advances the current spot through the list as long as 
+{							// current next is not null (end of the list)
+	Node<T>* tmp = head;
+	do
+	{
+		if (*(tmp->getItem()) == *(itemToCheck->getItem()))
+		{
+			return true;
+		}
+		if (tmp->getNext() != nullptr)
+		{
+			tmp = tmp->getNext();		
+		}
+	} while (tmp->getNext() != nullptr);
+
+	return false;
+}
+
+template<class T>
+bool LinkList<T>::isEmpty()
+{
+	if (head == nullptr)		
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+template<class T>
+int LinkList<T>::Size()
+{
+	return length;
 }
 
 template<class T>
