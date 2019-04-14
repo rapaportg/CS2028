2daa3fe181dcafa5f3046cb731e2f441d78b56c4
lslack <46945815+lslack@users.noreply.github.com>
2019-04-14 14:36:25 -0400

    update CHashTable

--- labs/Lab10/chainedHashTable.h
+++ labs/Lab10/chainedHashTable.h
@@ -11,33 +11,41 @@
 using namespace std;
 
 template<typename V>
-class ChainedHashTable: protected HashTable<V>
+class ChainedHashTable : protected HashTable<V>
 {
-    private:
-        LinkList<HashNode<V>>      *table;
-        int                        buckets;
-        int                        hashC(V val);
+private:
+	LinkList<HashNode<V>>      *table;
+	int                        buckets;
+	int                        hashC(V val);
+	int					    	testCounter2;
 
-    public:
-        ChainedHashTable();
-        ~ChainedHashTable();
-        void                      addItem(V val);
-        HashNode<V>               *removeItem(V val);
-        HashNode<V>               *getItem(V val);
-        int                       getLength();
-        void                      print();
+public:
+	ChainedHashTable();
+	~ChainedHashTable();
+	void                      addItem(V val);
+	HashNode<V>               *removeItem(V val);
+	HashNode<V>               *getItem(V val);
+	int                       getLength();
+	void                      print();
+	int						getTestCounter2();
 };
 
 template<typename V>
 ChainedHashTable<V>::ChainedHashTable()
 {
-    table = new LinkList<HashNode<V>>[TABLE_SIZE];
+	table = new LinkList<HashNode<V>>[TABLE_SIZE];
+}
+
+template<typename V>
+ChainedHashTable<V>::~ChainedHashTable()
+{
+	table->~LinkList();
 }
 
 template<typename V>
 int ChainedHashTable<V>::hashC(V val)
 {
-	int							ret;
+	int							ret = 0;
 	int							length;
 
 	length = val.length();
@@ -52,56 +60,81 @@ int ChainedHashTable<V>::hashC(V val)
 template<typename V>
 void ChainedHashTable<V>::addItem(V val)
 {
-    int index = hashC(val);
-    Node<HashNode<V>> *temp = new Node<HashNode<V>>();
-    HashNode<V>       *lol = new HashNode<V>(val);
-    temp->setItem(lol);
-    table[index].addItem(temp);
-    if (table[index].size() == 1)
-        buckets++;
+	int index = hashC(val);
+	Node<HashNode<V>> *temp = new Node<HashNode<V>>();
+	HashNode<V>       *lol = new HashNode<V>(val);
+	temp->setItem(lol);
+	table[index].addItem(temp);
+	if (table[index].size() == 1)
+		buckets++;
 }
 
 template<typename V>
 HashNode<V>   *ChainedHashTable<V>::removeItem(V val)
 {
-    int         i;
-    int         index = hashC(val);
+	int         i;
+	int         index = hashC(val);
 
-    for (i = 0; i < table[index].size(); i++)
-    {
-        if (table[index].seeAt(i)->getVal() == val)
-        {
-            return table[index]->remove(i);
-        }
+	for (i = 0; i < table[index].size(); i++)
+	{
+		if (table[index].seeAt(i)->getItem()->getVal() == val)
+		{
+			return table[index].remove(i)->getItem();
+		}
+	}
+	cout << "That item is not in the Hash Table! " << endl;
+	return nullptr;
+}
 
-    }
+template<typename V>
+HashNode<V>   *ChainedHashTable<V>::getItem(V val)
+{
+	int         i;
+	int         index = hashC(val);
+
+	for (i = 0; i < table[index].size(); i++)
+	{
+		if (table[index].seeAt(i)->getItem()->getVal() == val)
+		{
+			testCounter2++;
+			return table[index].seeAt(i)->getItem();
+		}
+	}
+	cout << "That item is not in the Hash Table! " << endl;
+	return nullptr;
 }
 
 template<typename V>
 int     ChainedHashTable<V>::getLength()
 {
-    int total;
+	int total = 0;
 
-    for (int i = 0; i < TABLE_SIZE; i++)
-    {
-        total += table[i]->size();
-    }
+	for (int i = 0; i < TABLE_SIZE; i++)
+	{
+		total += table[i].size();
+	}
 
-    cout << "Buckets: " << buckets << "\nTotal Number of Items: " << total << endl;
-
+	cout << "Buckets: " << buckets << "\nTotal Number of Items: " << total << endl;
+	return total;
 }
 
 template<typename V>
 void    ChainedHashTable<V>::print()
 {
-    for (int i = 0; i < TABLE_SIZE; i++)
-    {
-        cout << "Bucket: " << i << ".\t";
-        for (int j = 0; j < table[i].size(); j++)
-        {
-            cout << "---> " << table[i].seeAt(j)->getItem()->getVal();
-        }
-        cout << endl;
-    }
+	for (int i = 0; i < TABLE_SIZE; i++)
+	{
+		cout << "Bucket: " << i << ".\t";
+		for (int j = 0; j < table[i].size(); j++)
+		{
+			cout << "---> " << table[i].seeAt(j)->getItem()->getVal();
+		}
+		cout << endl;
+	}
+}
+
+template<typename V>
+int		ChainedHashTable<V>::getTestCounter2()
+{
+	return testCounter2;
 }
 #endif
